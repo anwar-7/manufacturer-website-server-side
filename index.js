e314100322a9124fe06b53ffb3063e6ef4a826b2
anwarul1 <anwarjewel007@gmail.com>
2022-05-24 00:37:19 +0600

    cunnect mongodb and make a function for api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
+// came with mongodb API
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -16,7 +19,22 @@ app.options('*', cors(corsConfig));
 app.use(express.json());
 
 // mongodb
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2kvc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db('ztools').collection('tools');
+  } finally {
+    // if need to close the server
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
